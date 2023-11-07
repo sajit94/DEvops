7d7fdfd51889c29a8da46beffee59a47c55e8739
unknown <sajith.sajith44@gmail.com>
2023-11-07 16:00:48 +0530

    resolved

--- linux/script.sh
+++ linux/script.sh
@@ -2,7 +2,4 @@
 #!/bin/bash
 <<<<<<< HEAD
 echo "Hello, updated from  github"
-echo "Hello,updated from git"
-=======
-echo "Hello, updated from  github!!!"
->>>>>>> c4c9c0f0715c57f498f79f8586ad60c03ec441b8
+
